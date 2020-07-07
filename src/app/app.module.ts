5b3e541d04070fb698a12018d8997e24bb0dde2a
ALBERTUS <TO127440@eu.airbus.corp>
2020-07-07 18:29:14 +0200

    Why we need to declare as entryComponent the MatDialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,9 @@ import { CourseDialogComponent } from './course-dialog/course-dialog.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
+  //To be able to use the component as a dialog body, we need to declare it as an entryComponent
+  //Otherwise, we will get the following error while opening the dialog :
+  //Error: no component factory found for CourseDialogComponent. Did you add it to @NgModule.entryComponents?
   entryComponents: [CourseDialogComponent]
 })
 export class AppModule { }
