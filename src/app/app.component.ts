642dfcf2e2208c37a73983960715323b87d8e147
ALBERTUS <TO127440@eu.airbus.corp>
2020-07-07 18:35:32 +0200

    Create and open an angular material dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-material-dialog';
+
+  constructor(private dialog: MatDialog) {}
+
+  openDialog() {
+
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    //Other options: hasBackdrop, panelClass, backdropClass, position, direction, closeOnNavigation
+
+    this.dialog.open(CourseDialogComponent, dialogConfig);
+  }
 }
